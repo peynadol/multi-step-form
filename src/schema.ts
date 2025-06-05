d88e146af9605b0b602193d080c3b00e13af4443
peynadol <peynadol@gmail.com>
2025-06-05 13:37:33 +0100

    add basic validation for phone number

--- src/schema.ts
+++ src/schema.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 export const formSchema = z.object({
   name: z.string().min(1),
   email: z.string().email(),
-  phoneNumber: z.string().min(1),
+  phoneNumber: z
+    .string()
+    .min(1, "Phone number is required")
+    .regex(/^\d+$/, "Phone number must contain only digits"),
   plan: z.enum(["Arcade", "Advanced", "Pro"]),
   billing: z.enum(["monthly", "yearly"]),
   addons: z.array(
